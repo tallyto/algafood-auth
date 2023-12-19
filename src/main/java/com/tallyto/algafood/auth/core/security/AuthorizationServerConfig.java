afe830024e64815698a32e306a2c8813434316a4
tallyto <rodrigues.tallyto@hotmail.com>
2023-12-19 09:12:08 -0300

    Configurando o Client Credentials Grant Type no Authorization Server

--- src/main/java/com/tallyto/algafood/auth/core/security/AuthorizationServerConfig.java
+++ src/main/java/com/tallyto/algafood/auth/core/security/AuthorizationServerConfig.java
@@ -34,6 +34,11 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                     .accessTokenValiditySeconds(6 * 60 * 60) // 6 horas de validade do token de acesso. (em segundos)
                     .refreshTokenValiditySeconds(12 * 60 * 60) // 12 horas de validade do token de refresh. (em segundos)
                 .and()
+                    .withClient("faturamento")
+                    .secret(passwordEncoder.encode("faturamento123"))
+                    .authorizedGrantTypes("client_credentials")
+                    .scopes("write", "read")
+                .and()
                     .withClient("admin")
                     .secret(passwordEncoder.encode("admin"));
 
